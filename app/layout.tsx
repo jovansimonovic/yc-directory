1c219ba55a26288795d568d9e7999fcb6e1524aa
Jovan Simonovic <jovan.simonovic02@gmail.com>
2025-01-24 16:23:52 +0100

    feature: added WorkSans font, configured tailwind

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,47 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const workSans = localFont({
+  src: [
+    {
+      path: "./fonts/WorkSans-Thin.ttf",
+      weight: "100",
+    },
+    {
+      path: "./fonts/WorkSans-ExtraLight.ttf",
+      weight: "200",
+    },
+    {
+      path: "./fonts/WorkSans-Light.ttf",
+      weight: "300",
+    },
+    {
+      path: "./fonts/WorkSans-Regular.ttf",
+      weight: "400",
+    },
+    {
+      path: "./fonts/WorkSans-Medium.ttf",
+      weight: "500",
+    },
+    {
+      path: "./fonts/WorkSans-SemiBold.ttf",
+      weight: "600",
+    },
+    {
+      path: "./fonts/WorkSans-Bold.ttf",
+      weight: "700",
+    },
+    {
+      path: "./fonts/WorkSans-ExtraBold.ttf",
+      weight: "800",
+    },
+    {
+      path: "./fonts/WorkSans-black.ttf",
+      weight: "900",
+    },
+  ],
+  variable: "--font-work-sans",
 });
 
 export const metadata: Metadata = {
@@ -24,11 +56,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={`${workSans.variable} antialiased`}>{children}</body>
     </html>
   );
 }
