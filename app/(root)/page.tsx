af9f229edff03f8f3c6ac00c7046fa413a89aa55
Jovan Simonovic <jovan.simonovic02@gmail.com>
2025-01-25 12:08:42 +0100

    refactor: move Navbar to another folder

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,23 @@
-export default function Home() {
+import SearchForm from "../../components/SearchForm";
+
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: { query?: string };
+}) {
+  const query = (await searchParams).query;
+
   return (
     <>
-      <h1 className="text-2xl">Home</h1>
+      <section className="pink-container">
+        <h1 className="heading">
+          Pitch Your Startup, <br /> Connect With Entrepeneurs
+        </h1>
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote on Pitches and Get Noticed in Virtual Competitions
+        </p>
+        <SearchForm query={query} />
+      </section>
     </>
   );
 }
